ccb9ec32d141dbd063d87c323741dcc49bcd5871
kevinrichard31 <30445326+kevinrichard31@users.noreply.github.com>
2023-11-22 12:31:12 +0000

    Fetch Post in Modal

--- api.ts
+++ api.ts
@@ -7,3 +7,16 @@ export const getAllTodos = async(): Promise<ITasks[]> => {
     const todos = await res.json();
     return todos;
 }
+
+export const addTodo = async(todo:ITasks): Promise<ITasks[]> => {
+    const res = await fetch(`${baseUrl}/tasks`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    });
+    const newTodo = await res.json();
+    return newTodo;
+
+}
--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,13 +1,33 @@
 "use client"; // This is a client component 👈🏽
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
 import Modal from "./Modal";
+import { addTodo } from "@/api";
 const AddTask = () => {
     const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [newTaskvalue, setNewTaskValue] = useState<string>('');
+    const handleSubmitNewTodo:FormEventHandler<HTMLFormElement> = 
+    async (e) => {
+        e.preventDefault();
+        await addTodo({
+            id: "3",
+            text: newTaskvalue
+        })
+        setNewTaskValue("");
+    }
 
     return <div className="w-full">
         <button className="btn btn-primary w-full" onClick={() => {setModalOpen(true); console.log(modalOpen)}}>Add new task <FaPlusCircle /></button>
-    <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+    <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} >
+        <form onSubmit={handleSubmitNewTodo}>
+            <h3 className="font-bold text-lg">Add new task</h3>
+            <div className="modal-action">
+            <input value={newTaskvalue} onChange={(e) => setNewTaskValue(e.target.value)} type="text" placeholder="Type here" className="input input-bordered w-full" />
+            <button type="submit" className="btn">Submit</button>
+            </div>
+        
+        </form>
+        </Modal>
     </div>
 }
 
